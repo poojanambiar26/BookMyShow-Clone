7d369d8e50164daea659bca053444bacc033bd23
poojanambiar26 <poojanambiar2002@gmail.com>
2023-04-22 12:42:09 +0530

    navColor added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        navColor:
+        
+{
+  50: '#e5f1ff',
+  100: '#bdd4f5',
+  200: '#93b7eb',
+  300: '#689be3',
+  400: '#417edb',
+  500: '#2b65c2',
+  600: '#204e98',
+  700: '#16386c',
+  800: '#0a2242',
+  900: '#000b1a',
+},
+
+
+      }
+    },
   },
   plugins: [],
 }
