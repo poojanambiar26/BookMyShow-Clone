727424e0ce29bb3a6c7ae2bb5a79a92df3761f41
poojanambiar26 <poojanambiar2002@gmail.com>
2023-04-22 16:18:20 +0530

    added hero carousels

--- src/layouts/default_layout.js
+++ src/layouts/default_layout.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "../Components/Navbar/navbar_component";
+import HeroCarousel from "../Components/HeroCarousel/HeroCarousel";
 const DefaultLayout = (props) => {
   return (
     <>
       <Navbar />
+      <HeroCarousel/>
       {/* <div>{props.children}</div> */}
     </>
   );
