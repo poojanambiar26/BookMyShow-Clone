33bab391097fed3e7cfc08e07745f6c5bc79da84
poojanambiar26 <poojanambiar2002@gmail.com>
2023-04-24 20:28:32 +0530

    config settings

---
+++ src/config/tempPosters.config.js
@@ -0,0 +1,72 @@
+const PremierImages=[
+    {
+    src:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OS41LzEwICA3LjRLIHZvdGVz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00345849-pelkursyjj-portrait.jpg",
+    alt:"Chengiz",
+    title:"Chengiz",
+    subtitle:"Action/Crime/Period"
+    },
+    {
+        src:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-Ni44LzEwICAzMi42SyB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00339118-hsjjwmgamf-portrait.jpg",
+        alt:"Kisi ka Bhai ",
+        title:"Kisi ka Bhai",
+        subtitle:"Action/Comedy/Drama"
+    },
+    {
+        src:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC4xLzEwICA0LjRLIHZvdGVz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00349309-kzclvwxuhc-portrait.jpg",
+        alt:"Evil Dead Rise",
+        title:"Evil Dead Rise",
+        subtitle:"Fantasy/horror/Thriller"
+
+    },
+    {
+        src:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@like_202006280402.png,ox-24,oy-617,ow-29:ote-MTE5LjlLIGxpa2Vz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00122562-snxucamjfy-portrait.jpg",
+        alt:"Fast X",
+        title:"Fast X",
+        subtitle:"Action/Adventure/Crime"
+
+    },
+    {
+        src:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OS4xLzEwICA1LjVLIHZvdGVz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00343051-hvakxzwsal-portrait.jpg",
+        alt:"Super Mario",
+        title:"Super Mario",
+        subtitle:"Adventure/Animation"
+
+    },
+    {
+        src:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OS41LzEwICAzLjRLIHZvdGVz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00352085-aghrdlnkqp-portrait.jpg",
+        alt:"Suzume",
+        title:"Suzume",
+        subtitle:"Action/Adventure/Anime"
+
+    },
+    {
+        src:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-Ny4yLzEwICA5LjRLIHZvdGVz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00353346-mwvmqqblwt-portrait.jpg",
+        alt:"The Pope's Exorcist",
+        title:"The Pope's Exorcist",
+        subtitle:"Horror/Thriller"
+
+    },
+    {
+        src:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-Ny43LzEwICA0Ny41SyB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00326977-szmnufqdzn-portrait.jpg",
+        alt:"Bholaa",
+        title:"Bholaa",
+        subtitle:"Action/Drama/Thriller"
+
+    },
+    {
+        src:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-Ny4yLzEwICAyMzMgdm90ZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00357595-tetbdfsuqu-portrait.jpg",
+        alt:"Trailer Screening show",
+        title:"Trailer Screening show",
+        subtitle:"Drama"
+
+    },
+    {
+        src:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-Ny43LzEwICA4Ni45SyB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00347237-bgdrxtrhxz-portrait.jpg",
+        alt:"Tu Jhoothi Main Makkaar",
+        title:"Tu Jhoothi Main Makkaar",
+        subtitle:"Comedy/Romantic"
+
+    }
+]
+
+export default PremierImages;
