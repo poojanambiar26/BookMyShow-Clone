399c769f10d90d66c8bca7271031920c24209d12
poojanambiar26 <poojanambiar2002@gmail.com>
2023-04-25 20:04:07 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Route } from "react-router-dom";
 import DefaultHOC from "./HOC/default_hoc";
-
+import MovieHOC from "./HOC/movie.hoc";
 //Pages
 import HomePage from "./Pages/Home_page";
-
+import MoviePage from "./Pages/movie_page";
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -14,6 +14,7 @@ function App() {
   
     <Route>
     <DefaultHOC path ="/" exact component={HomePage}/>
+    <MovieHOC path ="/movie/:id" exact component={MoviePage}/>
     </Route>
    
   );
