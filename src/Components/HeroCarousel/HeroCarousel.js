3013dae70fdd817886781f5dc5da98bb49c8432f
poojanambiar26 <poojanambiar2002@gmail.com>
2023-04-28 21:11:08 +0530

    TMDB API integration

--- src/Components/HeroCarousel/HeroCarousel.js
+++ src/Components/HeroCarousel/HeroCarousel.js
@@ -1,75 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import HeroSlider from "react-slick";
 import { NextArrow, PrevArrow } from "./arrows";
+import axios from "axios";
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const HeroCarousel = () =>{
-    const settingsLg ={
-        arrows: true,
-        autoplay: true,
-        centerMode: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        centerPadding: "300px",
-        infinite: true,
-        nextArrow: <NextArrow/>,
-        prevArrow: <PrevArrow/>
-    }
-    const settings = {
-        arrows:true,       
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        nextArrow: <NextArrow/>,
-        prevArrow: <PrevArrow/>
-      };
+const HeroCarousel = () => {
+  const [images, setImages] = useState([]);
 
-      const images =[
-    
-        "https://images.unsplash.com/photo-1681549734363-5c8a06428d5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80",
-        "https://images.unsplash.com/photo-1681318655864-843a37484f91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-        "https://images.unsplash.com/photo-1681151510371-3f07fd29235a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-        "https://images.unsplash.com/photo-1681609092173-913a55a786c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-        "https://images.unsplash.com/photo-1681581718209-f9f23be7f679?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80"
-      ]
-      return(
-        <>
-        <div className="lg:hidden">
+  useEffect(() => {
+    const requestNowPlayingMovies = async () => {
+      const getImages = await axios.get("/movie/now_playing");
+      setImages(getImages.data.results);
+    };
+    requestNowPlayingMovies();
+  }, []);
+  const settingsLg = {
+    arrows: true,
+    autoplay: true,
+    centerMode: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerPadding: "300px",
+    infinite: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
+  const settings = {
+    arrows: true,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
+
+  return (
+    <>
+      <div className="lg:hidden">
         <HeroSlider {...settings}>
-            {
-                images.map((image)=> (
-                    <div className="w-full h-64 md:h-80 py-3">
-                        <img src={image} alt="testing" className="w-full h-full rounded-sm"/>
-                    </div>
-                ))
-
-            }
+          {images.map((image) => (
+            <div className="w-full h-64 md:h-80 py-3">
+              <img src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
+                alt="testing"
+                className="w-full h-full rounded-sm"
+              />
+            </div>
+          ))}
         </HeroSlider>
-        </div>
+      </div>
 
-        <div className="hidden lg:block">
+      <div className="hidden lg:block">
         <HeroSlider {...settingsLg}>
-            {
-                images.map((image)=> (
-                    <div className="w-full h-96 px-2 py-3">
-                        <img src={image} alt="testing" className="w-full h-full rounded-sm"/>
-                    </div>
-                ))
-
-            }
+          {images.map((image) => (
+            <div className="w-full h-96 px-2 py-3">
+              <img
+                src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
+                alt="testing"
+                className="w-full h-full rounded-sm"
+              />
+            </div>
+          ))}
         </HeroSlider>
-        </div>
-
-        </>
-      )
+      </div>
+    </>
+  );
 };
 
 export default HeroCarousel;
-
-
 
 // http://kenwheeler.github.io/slick/
--- src/Pages/Home_page.js
+++ src/Pages/Home_page.js
@@ -1,41 +1,66 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import EntertainmentCardSlider from "../Components/Entertainment/EntertainmentCard";
 import Premier from "../Components/Premier/Premier";
 import PosterSlider from "../Components/PosterSlider/PosterSlider.comp";
 import PremierImages from "../config/tempPosters.config";
 
 const HomePage = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    const requestPopularMovies = async () => {
+      const getPopularMovies = await axios.get("/movie/popular");
+      setPopularMovies(getPopularMovies.data.results);
+    };
+    requestPopularMovies();
+  }, []);
+
+  console.log({ popularMovies });
+
   return (
     <>
-    
-   <div className="flex flex-col gap-5">
-   <div className="container mx-auto px-4">
-      <h1 className="text-2xl font-bold text-gray-800 px-4 py-4">
-         The best of Entertainment
-      </h1>
-      <EntertainmentCardSlider />
-      
-   </div>
-   <div className="bg-navColor-50 py-16">
-    
-    <div className="container mx-auto px-4">
-    <div className="flex">
-<img src="https://assets-in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120:q-80/premiere-banner-web-collection-202208191200.png"
-alt="rupay" className="w-full h-full"/>
-    </div>
-    <PosterSlider images={PremierImages} title="Premiers" subtitle ="Brand new Releases every Friday" isDark/>
-    </div>
-   </div>
-   </div>
-   <div className="container mx-auto px-4">
-    <PosterSlider images={PremierImages} title="Online Streaming Events" isDark={false}/>
-   </div>
+      <div className="flex flex-col gap-5">
+        <div className="container mx-auto px-4">
+          <h1 className="text-2xl font-bold text-gray-800 px-4 py-4">
+            The best of Entertainment
+          </h1>
+          <EntertainmentCardSlider />
+        </div>
+        <div className="bg-navColor-50 py-16">
+          <div className="container mx-auto px-4">
+            <div className="flex">
+              <img
+                src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
+                className="w-full h-full"
+              />
+            </div>
+            <PosterSlider
+              images={popularMovies}
+              title="Premiers"
+              subtitle="Brand new Releases every Friday"
+              isDark
+            />
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto px-4">
+        <PosterSlider
+          images={popularMovies}
+          title="Online Streaming Events"
+          isDark={false}
+        />
+      </div>
 
-   <div className="container mx-auto px-4">
-    <PosterSlider images={PremierImages} title="Outdoor Events" isDark={false}/>
-   </div>
+      <div className="container mx-auto px-4">
+        <PosterSlider
+          images={popularMovies}
+          title="Outdoor Events"
+          isDark={false}
+        />
+      </div>
     </>
-  )
+  );
 };
 
 export default HomePage;
